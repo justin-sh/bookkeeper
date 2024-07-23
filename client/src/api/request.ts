bb080f821d7a13da602a928301a2c6d45502caae
Justin LI <lijt2007@hotmail.com>
2024-07-23 13:38:07 +0930

    add api interface for register

--- client/src/api/request.ts
+++ client/src/api/request.ts
@@ -5,4 +5,6 @@ import {type User, type Account, type LoginForm} from "@/api/interface";
 
 export const userLogin = (data: LoginForm) => axios.post<User>('/auth/login', data)
 
+export const userRegister = (data: LoginForm) => axios.post<User>('/auth/register', data)
+
 export const getAccounts = () => axios.get<Account[]>('/accounts')
